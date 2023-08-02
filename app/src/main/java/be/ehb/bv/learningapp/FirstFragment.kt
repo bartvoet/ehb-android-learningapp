5c68373fe54f3f38116165f7ab900766cff6861f
Bart Voet <bart_voet@telenet.be>
2023-08-02 22:07:23 +0200

    Adding edittext dynamically

--- app/src/main/java/be/ehb/bv/learningapp/FirstFragment.kt
+++ app/src/main/java/be/ehb/bv/learningapp/FirstFragment.kt
@@ -1,44 +1,57 @@
 package be.ehb.bv.learningapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import be.ehb.bv.learningapp.databinding.FragmentFirstBinding
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        val binding = inflater.inflate(R.layout.fragment_first, container, false)
+        val linearLayout = binding.findViewById<LinearLayout>(R.id.linearLayout)
+        val rowView = inflater.inflate(R.layout.field, null)
+        //linearLayout.addView(rowView)
+        // Add the new row before the add field button.
+        val ET = EditText(activity)
+        ET.setText("LOL")
+        ET.id = 5
+        ET.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        linearLayout.addView(ET)
+        return binding
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //findViewById(R.id.linearLayout);
+        //linearLayout.addView()
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
