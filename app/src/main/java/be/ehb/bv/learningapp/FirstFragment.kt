d4f636f35a119ed17bd391a8904ee3e57e637375
Bart Voet <bart_voet@telenet.be>
2023-08-08 21:38:44 +0200

    Linking questiofragment with endfragment

--- app/src/main/java/be/ehb/bv/learningapp/FirstFragment.kt
+++ app/src/main/java/be/ehb/bv/learningapp/FirstFragment.kt
@@ -8,40 +8,36 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import be.ehb.bv.learningapp.databinding.FragmentFirstBinding
+import be.ehb.bv.learningapp.databinding.FragmentSecondBinding
 import java.util.logging.Logger
 
-
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
+    private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-
-
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = inflater.inflate(R.layout.fragment_first, container, false)
-        val linearLayout = binding.findViewById<LinearLayout>(R.id.linearLayout)
-        //val rowView = inflater.inflate(R.layout.field, null)
-        //linearLayout.addView(rowView)
-        // Add the new row before the add field button.
+        //_binding = inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val linearLayout = binding.root.rootView as LinearLayout //
+
         for(i in 1..5) {
             addEditText(i.toString(), linearLayout)
         }
-        return binding
+        return binding.root
 
     }
 
     private fun addEditText(text: String, linearLayout: LinearLayout) {
         val ET = EditText(activity)
-        ET.setText("LOL")
+        ET.setText("LOL!!")
         ET.setOnClickListener { Log.i("test", text) }
         ET.layoutParams = ViewGroup.LayoutParams(
             ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -52,12 +48,10 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //findViewById(R.id.linearLayout);
-        //linearLayout.addView()
 
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
     }
 
     override fun onDestroyView() {
