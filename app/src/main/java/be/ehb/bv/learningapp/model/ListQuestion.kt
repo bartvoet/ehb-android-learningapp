b6d7af75a74546a0bffc65c2f31f11303fda0f85
Bart Voet <bart_voet@telenet.be>
2023-08-12 00:15:57 +0200

    Activate validation in listquestion

--- app/src/main/java/be/ehb/bv/learningapp/model/ListQuestion.kt
+++ app/src/main/java/be/ehb/bv/learningapp/model/ListQuestion.kt
@@ -1,16 +1,16 @@
 package be.ehb.bv.learningapp.model
 
+fun <T> List<T>.equalsIgnoreOrder(other: List<T>) = this.size == other.size && this.toSet() == other.toSet()
+
 data class ListQuestion(private val question : String, private val answers: List<String>) : Question {
 
     override fun ask(qi: Question.QuestionInterface) {
         qi.askListQuestion(question, answers.size)
     }
-
     override fun validate(qi: Question.QuestionInterface): Question.QuestionFeedback {
-        //val answers = qi.getFeedback()
-        // TODO: compare lists
+        val receivedAnswers = qi.getFeedback()
         return object : Question.QuestionFeedback {
-            override fun isOK() = true
+            override fun isOK() = receivedAnswers.equalsIgnoreOrder(answers)
         }
     }
 }
