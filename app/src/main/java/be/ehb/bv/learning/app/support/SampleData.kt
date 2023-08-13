3a47c9b457f1391da820714a9ce177603ef105c2
Bart Voet <bart_voet@telenet.be>
2023-08-14 00:09:58 +0200

    Adding sample data

--- app/src/main/java/be/ehb/bv/learning/app/support/SampleData.kt
+++ app/src/main/java/be/ehb/bv/learning/app/support/SampleData.kt
@@ -6,14 +6,28 @@ import be.ehb.bv.learning.core.model.Question
 object SampleData {
     val SAMPLE_QUESTIONS  =
         mapOf (
-            "networking"  to listOf(
-                ListQuestion("Wat zijn de poorten voor HTTP en HTTPS?", listOf("80", "443")),
-                ListQuestion("Waar staat DORA voor?", listOf("Discover", "Offer", "Request", "Accept"))
+            "network, port en protocol" to listOf(
+                ListQuestion("FTP", listOf("20","TCP")),
+                ListQuestion("FTP Control", listOf("21","TCP")),
+                ListQuestion("SSH:", listOf("22","TCP")),
+                ListQuestion("Telnet", listOf("23","TCP")),
+                ListQuestion("DNS", listOf("53","UDP","TCP")),
+                ListQuestion("DHCP Server", listOf("67","UDP")),
+                ListQuestion("DHCP Client", listOf("68","UDP")),
+                ListQuestion("TFTP", listOf("69","UDP")),
+                ListQuestion("HTTP", listOf("80","TCP")),
+                ListQuestion("SNMP", listOf("161","UDP")),
+                ListQuestion("SNMP-trap", listOf("162","UDP")),
+                ListQuestion("LDAP", listOf("389","TCP")),
+                ListQuestion("HTTPS",listOf("443","TCP")),
+                ListQuestion("DHCP Client (ipv6)",listOf("546","UDP")),
+                ListQuestion("DHCP Server (ipv6)",listOf("547","UDP"))
             ) as List<Question>,
             "programming" to listOf(
                 ListQuestion("Wat zijn de favoriete programmeertalen?", listOf("Java", "Kotlin","Rust", "C")),
                 ListQuestion("Wat zijn de 2 keywords in Kotlin om een variabele te definieren??",
-                                    listOf("val", "var")),
+                    listOf("val", "var")),
             ) as List<Question>
-        )
+
+            )
 }
