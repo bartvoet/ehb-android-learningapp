0cc7f23cdfd8f76d2ad240051d54bc683aab2ad6
Bart Voet <bart_voet@telenet.be>
2023-08-13 16:20:46 +0200

    Connecting resource-service to selectactivity

--- app/src/main/java/be/ehb/bv/learning/app/start/SelectActivity.kt
+++ app/src/main/java/be/ehb/bv/learning/app/start/SelectActivity.kt
@@ -1,10 +1,16 @@
 package be.ehb.bv.learning.app.start
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import be.ehb.bv.learning.app.databinding.ActivitySelectBinding
+import be.ehb.bv.learning.app.service.QuestionResourceService
 import be.ehb.bv.learning.app.session.QuestionActivity
+
 
 class SelectActivity : AppCompatActivity() {
 
@@ -17,5 +23,30 @@ class SelectActivity : AppCompatActivity() {
             startActivity(Intent(this, QuestionActivity::class.java))
         }
         setContentView(binding.root)
+    }
+
+    private var boundService : QuestionResourceService? = null
+    private var isBound = false
+
+    override fun onStart() {
+        super.onStart()
+        val intent = Intent(this, QuestionResourceService::class.java)
+        startService(intent)
+        bindService(intent, boundServiceConnection, BIND_AUTO_CREATE)
+    }
+
+    private val boundServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binderBridge: QuestionResourceService.LocalBinder =
+                service as QuestionResourceService.LocalBinder
+            boundService = binderBridge.getService()
+            isBound = true
+            Log.i("test", "connected!!!!")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            isBound = false
+            boundService = null
+        }
     }
 }
