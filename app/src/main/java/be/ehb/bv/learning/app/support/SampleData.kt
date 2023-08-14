e5a1d9296c485a2f173a060ab6e4ed2bb8be261a
Bart Voet <bart.voet@cegeka.com>
2023-08-14 12:36:42 +0200

    Making questions for sampledata more clear

--- app/src/main/java/be/ehb/bv/learning/app/support/SampleData.kt
+++ app/src/main/java/be/ehb/bv/learning/app/support/SampleData.kt
@@ -7,21 +7,21 @@ object SampleData {
     val SAMPLE_QUESTIONS  =
         mapOf (
             "network, port en protocol" to listOf(
-                ListQuestion("FTP", listOf("20","TCP")),
-                ListQuestion("FTP Control", listOf("21","TCP")),
-                ListQuestion("SSH:", listOf("22","TCP")),
-                ListQuestion("Telnet", listOf("23","TCP")),
-                ListQuestion("DNS", listOf("53","UDP","TCP")),
-                ListQuestion("DHCP Server", listOf("67","UDP")),
-                ListQuestion("DHCP Client", listOf("68","UDP")),
-                ListQuestion("TFTP", listOf("69","UDP")),
-                ListQuestion("HTTP", listOf("80","TCP")),
-                ListQuestion("SNMP", listOf("161","UDP")),
-                ListQuestion("SNMP-trap", listOf("162","UDP")),
-                ListQuestion("LDAP", listOf("389","TCP")),
-                ListQuestion("HTTPS",listOf("443","TCP")),
-                ListQuestion("DHCP Client (ipv6)",listOf("546","UDP")),
-                ListQuestion("DHCP Server (ipv6)",listOf("547","UDP"))
+                ListQuestion("Port and trasport: FTP", listOf("20","TCP")),
+                ListQuestion("Port and trasport: FTP Control", listOf("21","TCP")),
+                ListQuestion("Port and trasport: SSH:", listOf("22","TCP")),
+                ListQuestion("Port and trasport: Telnet", listOf("23","TCP")),
+                ListQuestion("Port and trasport: DNS", listOf("53","UDP","TCP")),
+                ListQuestion("Port and trasport: DHCP Server", listOf("67","UDP")),
+                ListQuestion("Port and trasport: DHCP Client", listOf("68","UDP")),
+                ListQuestion("Port and trasport: TFTP", listOf("69","UDP")),
+                ListQuestion("Port and trasport: HTTP", listOf("80","TCP")),
+                ListQuestion("Port and trasport: SNMP", listOf("161","UDP")),
+                ListQuestion("Port and trasport: SNMP-trap", listOf("162","UDP")),
+                ListQuestion("Port and trasport: LDAP", listOf("389","TCP")),
+                ListQuestion("Port and trasport: HTTPS",listOf("443","TCP")),
+                ListQuestion("Port and trasport: DHCP Client (ipv6)",listOf("546","UDP")),
+                ListQuestion("Port and trasport: DHCP Server (ipv6)",listOf("547","UDP"))
             ) as List<Question>,
             "programming" to listOf(
                 ListQuestion("Wat zijn de favoriete programmeertalen?", listOf("Java", "Kotlin","Rust", "C")),
